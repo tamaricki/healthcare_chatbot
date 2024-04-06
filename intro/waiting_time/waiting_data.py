2343820aa3eec76fa44b74dcfbddf5737b179b08
Tamara <tammiricki@gmail.com>
2024-04-06 12:01:28 +0200

    working on api

--- intro/waiting_time/waiting_data.py
+++ intro/waiting_time/waiting_data.py
@@ -6,14 +6,17 @@ import json
 import pandas as pd
 #%%
 
-url= 'https://sdmx.oecd.org/public/rest/data/OECD.ELS.HD,DSD_HEALTH_PROC@DF_WAITING,1.0/.WAIT_MEAN.............WTSP...?format=jsondata&dimensionAtObservation=AllDimensions'
+url= 'https://sdmx.oecd.org/public/rest/data/OECD.ELS.HD,DSD_HEALTH_PROC@DF_WAITING,1.0/.WAIT_MEAN.............WTSP...?format=jsondata&startPeriod=2018&dimensionAtObservation=AllDimensions'
 
+
+#to add countries (estonia, denmark example,startPeriod=2018)
+#https://sdmx.oecd.org/public/rest/data/OECD.ELS.HD,DSD_HEALTH_PROC@DF_WAITING,1.0/EST+DNK.WAIT_MEAN.............WTSP...?dimensionAtObservation=AllDimensions
 
 response = requests.get(url)
 response=response.json()
 
 # %%
-response
+response['data']['structures'] #['dimensions']['observation']
 # %%
 data = pd.read_csv('OECD_waiting_time.csv')
 
