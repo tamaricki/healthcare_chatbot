2c2c5830b3760b25cd6cacc6a4141ce1bc9b1aa5
Tamara <tammiricki@gmail.com>
2024-04-21 12:26:57 +0200

    move files, add query

---
+++ chat_api/api_models/hospital_rag_query.py
@@ -0,0 +1,13 @@
+
+from pydantic import BaseModel
+
+#this model verifies if POST request includes a text field - this is query to chatbot, on which bot respons
+class HospitalQueryInput(BaseModel):
+    text:str
+
+#this model verifies if response back to user includes input string, output and list of intermidiate steps 
+class HospitalQueryOutput(BaseModel):
+    input: str
+    output: str
+    intermidiate_steps: list[str]
+
